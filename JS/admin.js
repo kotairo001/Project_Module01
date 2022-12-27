721f1b2e098f3010c52cba07937fa3758670dfea
vangtt <giangthanhvan12@gmail.com>
2022-12-27 21:14:53 +0700

    update

---
+++ JS/admin.js
@@ -0,0 +1,11 @@
+function addAdminAccount() {
+  let adminAccount = {
+    email: "admin@gmail.com",
+    password: 1234567,
+    status: false,
+  };
+  let listAdmin = [];
+  listAdmin.push(adminAccount);
+  localStorage.setItem("adminAccount", JSON.stringify(listAdmin));
+}
+addAdminAccount();
